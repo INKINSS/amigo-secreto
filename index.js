902b8a3b53f80c0276befbf40b13a2fb7f8f4d3e
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-02-24 15:47:14 -0600

    feat: adds the button state based on the input content

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const input = document.getElementById("input");
 const list = document.getElementById("list");
 const button = document.getElementById("button");
 
+const stateButton = () => {
+  if (input.value.trim() === "") {
+    button.classList.add("disabled");
+    button.disabled = true; // Desactiva el botón
+  } else {
+    button.classList.remove("disabled");
+    button.disabled = false; // Activa el botón
+  }
+};
+
+stateButton()
+
+input.addEventListener("input", stateButton);
+
 const addFriendToList = () => {
   const text = input.value;
   const li = document.createElement("li");
   li.classList.add("item");
   li.textContent = text;
   list.appendChild(li);
-}
+  input.value = "";
+  stateButton()
+};
 
 const handleClick = () => {
   addFriendToList();
-}
+};
